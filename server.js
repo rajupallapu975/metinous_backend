d75b96216d0663d09d9d8cc5a2ca05721d74d498
clt99062-lang <clt99062@gmail.com>
2026-08-06 23:55:54 +0530

    Serve Flutter Web app on port 2134

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const WebSocket = require('ws');
 const cors = require('cors');
 const { exec } = require('child_process');
 const os = require('os');
+const path = require('path');
 const chatRouter = require('./routes/chat');
 const errorHandler = require('./middleware/errorHandler');
 const { runCognitivePipeline } = require('./nlp_ner/cognitiveArchitecture');
@@ -15,6 +16,9 @@ let PORT = parseInt(process.env.PORT || '2134', 10);
 
 app.use(cors());
 app.use(express.json());
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount API routes
 app.use('/api', chatRouter);
